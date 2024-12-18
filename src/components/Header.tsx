115aadcbc10822db0395b601e204fd35f78e60b4
odinokun <odinokun@gmail.com>
2024-12-18 18:29:54 -0500

    feat: add Header component with AppBar and Toolbar for navigation

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export const Header: FC = () => {
+  return (
+    <AppBar position='static'>
+      <Toolbar>
+        <IconButton size='large' edge='start' color='inherit' sx={{ mr: 2 }}>
+          <MenuIcon />
+        </IconButton>
+        <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+          News
+        </Typography>
+        <Button color='inherit'>Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
